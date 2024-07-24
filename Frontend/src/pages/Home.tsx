89ed0fc755406acbcc4620a37b8c1f49e8533195
kinshukgarg <37059829+kinshukgarg@users.noreply.github.com>
2024-07-24 07:16:23 -0700

    doing sass of home

---
+++ Frontend/src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom"
+
+const Home = () => {
+  return (
+    <div className="home">  
+    <section></section>
+    
+    <h1>latest Product
+     
+     <Link to='/search' className="findmore">More</Link>
+
+
+
+    </h1>
+
+
+   <main></main>
+    
+    
+    </div>
+  )
+  
+}
+
+export default Home
