e85ec745f128098fffe28499ede77102de00b5e6
kinshukgarg <37059829+kinshukgarg@users.noreply.github.com>
2024-07-28 23:02:44 -0700

    home saas is done now working on cart page

--- Frontend/src/pages/Cart.tsx
+++ Frontend/src/pages/Cart.tsx
@@ -1,8 +1,22 @@
-
+const cartItems=[];
+const subtotal=4000;
+const tax=Math.round(subtotal *0.18);
+const shippingCharges=200;
+const total=  subtotal+tax+shippingCharges;
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <div className="cart">
+      <main></main>
+      <aside> 
+     <p>subtotal:₹{subtotal}</p>
+     <p>shippingCharges:₹{shippingCharges}</p>
+     <p>tax:₹{tax}</p>
+     <p></p>
+      
+      
+      </aside>
+    </div>
   )
 }
 
