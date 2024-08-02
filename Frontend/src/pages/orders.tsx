a4a9506366ee273b5c9c762acd135cb637796f5f
kinshukgarg <37059829+kinshukgarg@users.noreply.github.com>
2024-08-02 00:12:34 -0700

    order page is complete

--- Frontend/src/pages/orders.tsx
+++ Frontend/src/pages/orders.tsx
@@ -1,8 +1,82 @@
+import { ReactElement, useState } from "react";
+import TableHOC from "../componets/admin/TableHOC"
+import {Link} from'react-router-dom'
+
+
+
+
+
+
+
+
+type DataType={
+_id:string;
+amount:number;
+quantity:number;
+discount:number;
+status:ReactElement;
+action:ReactElement;
+
+}
+const column: Column<DataType>[] = [
+    {
+      Header: "ID",
+      accessor: "_id",
+    },
+    {
+      Header: "Quantity",
+      accessor: "quantity",
+    },
+    {
+      Header: "Discount",
+      accessor: "discount",
+    },
+    {
+      Header: "Amount",
+      accessor: "amount",
+    },
+    {
+      Header: "Status",
+      accessor: "status",
+    },
+    {
+      Header: "Action",
+      accessor: "action",
+    },
+  ];
 
 
 const Orders = () => {
+    const [rows]=useState<DataType[]>([{
+        _id:"asss",
+        amount:4545,
+        quantity:23,
+        discount:5666,
+        status:<span className="red">Processing</span>,
+        action:<Link to={`/order/asss`}>Vieww</Link>,
+    }])
+
+const Table = TableHOC<DataType>(
+    column,
+    rows,
+    "dashboard-product-box",
+    "Orders",
+    rows.length > 6
+  )();
+
+
+
+
+
+
+
   return (
-    <div>orders</div>
+    <div className="container">
+  <h1>My orders</h1>
+  {Table}
+
+
+    </div>
   )
 }
 
