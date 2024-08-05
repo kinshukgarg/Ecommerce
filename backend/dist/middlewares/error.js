e8ba482d1c0b8d02e3b08b46c4f3a2fcf7b398e4
kinshukgarg <37059829+kinshukgarg@users.noreply.github.com>
2024-08-04 22:48:37 -0700

    status code error

--- backend/dist/middlewares/error.js
+++ backend/dist/middlewares/error.js
@@ -1,6 +1,6 @@
 export const errorMiddleware = (err, req, resp, next) => {
     err.message || (err.message = "Internal server error");
-    err.message || (err.message = "Internal server error");
+    err.statusCode || (err.statusCode = 500);
     return resp.status(400).json({
         success: false,
         message: "Some Error"
--- backend/src/middlewares/error.ts
+++ backend/src/middlewares/error.ts
@@ -1,15 +1,16 @@
 import { NextFunction, Request, Response } from "express";
+import ErrorHandler from "../utils/utility-class.js";
 
 
 
 
-export const errorMiddleware=(err:Error,req:Request,resp:Response,next:NextFunction)=>{
+export const errorMiddleware=(err:ErrorHandler,req:Request,resp:Response,next:NextFunction)=>{
 
 
 
 
     err.message ||="Internal server error"; 
-    err.message ||="Internal server error"; 
+    err.statusCode ||=500; 
     return resp.status(400).json({
         success:false,
         message:"Some Error"
