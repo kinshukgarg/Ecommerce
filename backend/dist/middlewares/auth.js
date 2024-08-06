6e363609f9ed6d2367f9268ef599ebd4fdb91d66
kinshukgarg <37059829+kinshukgarg@users.noreply.github.com>
2024-08-05 23:07:46 -0700

    working

--- backend/dist/middlewares/auth.js
+++ backend/dist/middlewares/auth.js
@@ -1,13 +1,15 @@
+import { User } from "../models/user.js";
 import ErrorHandler from "../utils/utility-class.js";
 import { TryCatch } from "./error.js";
-import { User } from "../models/user.js";
+// Middleware to make sure only admin is allowed
 export const adminOnly = TryCatch(async (req, res, next) => {
-    const { id } = req.quey;
+    const { id } = req.query;
     if (!id)
-        return next(new Errorhandler("wtf", 401));
+        return next(new ErrorHandler("Saale Login Kr phle", 401));
     const user = await User.findById(id);
     if (!user)
-        return next(new ErrorHandler("how dare to use fake id", 401));
+        return next(new ErrorHandler("Saale Fake ID Deta Hai", 401));
     if (user.role !== "admin")
-        return next(new ErrorHandler("how dare", 401));
+        return next(new ErrorHandler("Saale Aukat Nhi Hai Teri", 403));
+    next();
 });
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,20 +1,21 @@
 import express from "express";
-import { deleteUser, getAllUsers, getUser, newUser } from "../controllers/user.js";
+import {
+  deleteUser,
+  getAllUsers,
+  getUser,
+  newUser,
+} from "../controllers/user.js";
 import { adminOnly } from "../middlewares/auth.js";
 
 const app = express.Router();
 
-//route -/api/v1/user/new
-
+// route - /api/v1/user/new
 app.post("/new", newUser);
 
+// Route - /api/v1/user/all
+app.get("/all", adminOnly, getAllUsers);
 
-//route-api/v1/user/all
-app.get("/all",getAllUsers)
-
-
-//route-api/v1/user/dynamicid
-app.route("/:id").get(getUser).delete(adminOnly,deleteUser)
-
+// Route - /api/v1/user/dynamicID
+app.route("/:id").get(getUser).delete(adminOnly, deleteUser);
 
 export default app;
