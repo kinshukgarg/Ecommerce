d67d9a14aec6feb434073f545f435b2de79986bb
kinshukgarg <37059829+kinshukgarg@users.noreply.github.com>
2024-08-19 21:50:23 -0700

    update version

--- backend/dist/utils/feature.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-export const connectDb = () => {
-    mongoose.connect('mongodb+srv://admin:iBOK5TKcKGg4j7Zz@cluster0.oqosrqo.mongodb.net/', {
-        dbName: 'Ecommerce'
-    }).then(c => console.log(`DB connected to ${c.connection.host}`))
-        .catch(e => console.log(e));
-};
